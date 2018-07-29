0c12d1a0a1d28dae54c3f5e07041b8c5b0425968
John Murphy <murf2073@gmail.com>
2018-07-29 02:02:01 -0400

    psuedo for JS code later, structure planned v1

---
+++ main.js
@@ -0,0 +1,9 @@
+// grab all the elements and give variables for dry code
+//set up event listeners for functionality of total, check someones balance, request/add, etc
+//also needs private method for me to increase... actually maybe i should handle all balance my end
+//reach would be a way for both to send a signed message agreeing to trade and then i can push it through
+
+
+//one basics set up, have function that has array of hashes [{fname: "john", lname: "murphy ", transcations: []}, etc.]
+// or a counter balance thing from thee user - checks their balance, confirms who to send, reassigns it all on the ledger
+// or "ledger" thats just a method that likely needs to be private like a lot of this since in theory its currency
