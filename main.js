71708502920fd95f175998df00cbf8661e9a23b8
John Murphy <murf2073@gmail.com>
2018-07-29 01:29:36 -0400

    js psuedo code scaffolding

---
+++ main.js
@@ -0,0 +1,5 @@
+// event listners to grab all
+//set click listeners
+//place to store total and hash (UUID) to hold account totals
+//account page, devise for login do on heroku
+// or for now do front end site with total counter and list of all balances, with a request form to have me updated it if others perform a transaction they wanted processed.
