3e2e554c27952bbd24de58ba8dbb84dee4e459fb
John Murphy <murf2073@gmail.com>
2018-07-29 02:04:10 -0400

    js psuedocoded

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
-// event listners to grab all
-//set click listeners
-//place to store total and hash (UUID) to hold account totals
-//account page, devise for login do on heroku
-// or for now do front end site with total counter and list of all balances, with a request form to have me updated it if others perform a transaction they wanted processed.
+// grab all the elements and give variables for dry code
+//set up event listeners for functionality of total, check someones balance, request/add, etc
+//also needs private method for me to increase... actually maybe i should handle all balance my end
+//reach would be a way for both to send a signed message agreeing to trade and then i can push it through
+
+
+//one basics set up, have function that has array of hashes [{fname: "john", lname: "murphy ", transcations: []}, etc.]
+// or a counter balance thing from thee user - checks their balance, confirms who to send, reassigns it all on the ledger
+// or "ledger" thats just a method that likely needs to be private like a lot of this since in theory its currency
